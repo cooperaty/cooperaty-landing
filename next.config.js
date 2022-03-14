d0b484c544c9ded85f327540263812f06d1f6197
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2022-03-14 13:21:13 +0100

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  assetPrefix: isProd ? '/cooperaty-landing/' : '',
   target: 'serverless',
   webpack(config) {
     config.module.rules.push({
